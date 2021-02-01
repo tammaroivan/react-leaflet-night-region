c614f7447ccac97bb9dda50d65926f7702203b48
Ivan Tammaro <tammaroivan@gmail.com>
2021-02-01 15:50:10 +0100

    Updates

---
+++ precache-manifest.a981af43ef2212b65615ab6d0b56d432.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45078ad18a27e75ee77e967d47c52b7e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "97f471473ec46776a161",
+    "url": "./static/css/2.ddfbff33.chunk.css"
+  },
+  {
+    "revision": "eec9ccb88cf01bdd515f",
+    "url": "./static/css/main.5f8aea21.chunk.css"
+  },
+  {
+    "revision": "97f471473ec46776a161",
+    "url": "./static/js/2.e70d61ad.chunk.js"
+  },
+  {
+    "revision": "f5846a532341dbf81a6fe52f73022fce",
+    "url": "./static/js/2.e70d61ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eec9ccb88cf01bdd515f",
+    "url": "./static/js/main.e081a928.chunk.js"
+  },
+  {
+    "revision": "ff6e1d6b4052c1c6d123",
+    "url": "./static/js/runtime-main.2a40baf4.js"
+  }
+]);
